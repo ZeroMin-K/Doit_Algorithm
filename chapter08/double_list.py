13516db43ef49f2038b4265684bd88309a336cbb
ZeroMin-K <77002670+ZeroMin-K@users.noreply.github.com>
2021-03-16 23:31:23 +0900

    Update double_list.py

--- chapter08/double_list.py
+++ chapter08/double_list.py
@@ -26,3 +26,19 @@ class DoubleLinkedList:
     def is_empty(self) -> bool:
         # 리스트가 비어있는지 확인
         return self.head.next is self.head
+
+        def search(self, data: Any) -> Any:
+        # data와 값이 같은 노드 검색
+        cnt = 0
+        ptr = self.head.next        # 현재 스캔중인 노드
+        while ptr is not self.head:
+            if data == ptr.data:
+                self.current = ptr
+                return cnt          # 검색 성공
+            cnt += 1
+            ptr = ptr.next          # 뒤쪽 노드 주목
+        return -1                   # 검색 실패
+
+    def __contains__(self, data: Any) -> bool:
+        # 연결리스트에 data가 포함되어 있는지 판단
+        return self.search(data) >= 0   
