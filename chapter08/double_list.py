b80ce42ec67da9ba8dd364cd6af55d838c6996e5
ZeroMin-K <77002670+ZeroMin-K@users.noreply.github.com>
2021-03-17 23:23:56 +0900

    Update double_list.py

--- chapter08/double_list.py
+++ chapter08/double_list.py
@@ -42,3 +42,38 @@ class DoubleLinkedList:
     def __contains__(self, data: Any) -> bool:
         # 연결리스트에 data가 포함되어 있는지 판단
         return self.search(data) >= 0   
+
+        def print_current_node(self) -> None:
+        # 주목 노드 출력
+        if self.is_empty():
+            print('주목 노드는 없습니다')
+        else:
+            print(self.current.data)
+
+    def print(self) -> None:
+        # 모든 노드 출력
+        ptr = self.head.next    # 더미 노드의 뒤쪽 노드
+        while ptr is not self.head:
+            print(ptr.data)
+            ptr = ptr.next
+    
+    def print_reverse(self) -> None:
+        # 모든 노드를 역순으로 출력
+        ptr = self.head.prev    # 더미 노드의 앞쪽 노드
+        while ptr is not self.head:
+            print(ptr.data)
+            ptr = ptr.prev
+
+    def next(self) -> bool:
+        # 주목 노드를 한칸 뒤로 이동
+        if self.is_empty() or self.current.next is self.head:
+            return False
+        self.current = self.current.next
+        return True
+
+    def prev(self) -> bool:
+        # 주목 노드를 한 칸 앞으로 이동
+        if self.is_empty() or self.current.prev is self.head:
+            return False
+        self.current = self.current.prev
+        return True
