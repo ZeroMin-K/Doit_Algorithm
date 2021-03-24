1036c4180a783dd7a95be710fe6d3b58ef0d0f38
ZeroMin-K <77002670+ZeroMin-K@users.noreply.github.com>
2021-03-24 23:06:13 +0900

    Update bst.py

--- chapter09/bst.py
+++ chapter09/bst.py
@@ -31,3 +31,28 @@ class BinarySearchTree:
                 p = p.left
             else:
                 p = p.right 
+                
+     def add(self, key: Any, value: Any) -> bool:
+        # 키가 key이고 값이 value인 노드 삽입
+
+        def add_node(node: Node, key: Any, value: Any) -> None:
+            # node를 루트로 하는 서브트리에 키가 key이고 값이 value인 노드 삽입
+            if key == node.key:
+                return False
+            elif key < node.key:
+                if node.left is None:
+                    node.left = Node(key, value, None, None)
+                else:
+                    add_node(node.left, key, value)
+            else:
+                if node.right is None:
+                    node.right = Node(key, value, None, None)
+                else:
+                    add_node(node.right, key, value)
+            return True
+        
+        if self.root is None:
+            self.root = Node(key, value, None, None)
+            return True
+        else:
+            return add_node(self.root, key, value)
